aa41a1867e9b1460a056fc5b51e80ec43e4b3210
Jan Schulte <janschul@cisco.com>
2023-09-05 15:36:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/ping', (req, res) => {
+  const domain = req.body.domain;
+
+  exec(`ping -c 4 ${domain}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+
+    const output = stdout;
+    res.send(output);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
